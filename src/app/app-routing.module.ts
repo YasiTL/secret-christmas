f30236009f382995cc8e6015a0854afcafe6a427
YasiTL <yasaman.most@gmail.com>
2018-11-21 23:01:40 -0600

    full update of site, first problem in, side bar doesnt do much of anything, split flap display not really working/not implemented at all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SidenavComponent} from './sidenav/sidenav.component';
+import {LoginComponent} from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'main', component: SidenavComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {} from 'node_modules/angular-split-flap';
 
-import {MatButtonModule} from '@angular/material';
+
+
+import {
+  MatButtonModule, MatButtonToggleModule, MatIconModule, MatListModule,
+  MatSidenavModule
+} from '@angular/material';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainContentComponent } from './sidenav/main-content/main-content.component';
+import { LoginComponent } from './login/login.component';
+import {MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidenavComponent
+    SidenavComponent,
+    MainContentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    // ngSplitFlap,
     MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
