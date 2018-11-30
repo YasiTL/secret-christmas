634a6f06aa1bf416fb5ec29962519259431ea1ed
YasiTL <yasaman.most@gmail.com>
2018-11-30 09:05:04 -0600

    problem 1 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import {} from 'node_modules/angular-split-flap';
-
-
 
 import {
   MatButtonModule, MatButtonToggleModule, MatIconModule, MatListModule,
-  MatSidenavModule
+  MatSidenavModule, MatFormFieldModule, MatInputModule
 } from '@angular/material';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { MainContentComponent } from './sidenav/main-content/main-content.component';
 import { LoginComponent } from './login/login.component';
 import {MatToolbarModule} from '@angular/material';
+import { Problem2Component } from './sidenav/problem2/problem2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidenavComponent,
     MainContentComponent,
-    LoginComponent
+    LoginComponent,
+    Problem2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // ngSplitFlap,
     MatButtonModule,
     MatSidenavModule,
     MatListModule,
@@ -37,6 +35,8 @@ import {MatToolbarModule} from '@angular/material';
     MatButtonToggleModule,
     MatToolbarModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sidenav/problem2/problem2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-problem2',
+  templateUrl: './problem2.component.html',
+  styleUrls: ['./problem2.component.scss']
+})
+export class Problem2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
